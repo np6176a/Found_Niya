afbd249777eb616fb1ac448cb62d753404b123af
np6176a <niyamp3@gmail.com>
2015-03-08 17:13:22 -0400

    gulp bower and bootstrap connections

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var haml = require('gulp-haml');
+var notify = require("gulp-notify") ;
+var bower = require('gulp-bower');
 var watch = require('gulp-watch');
 
 gulp.task('default', function() {
@@ -25,4 +27,29 @@ gulp.task('default', function() {
         gulp.watch('./src/**/*.haml', ['haml']);
         gulp.watch('./src/styles/*.scss', ['scss']);
     });
+
+    gulp.task('bower', function() {
+        return bower()
+        .pipe(gulp.dest(config.bowerDir))
+    });
+
+    gulp.task('css', function() {
+    return gulp.src(config.sassPath + '/style.scss')
+        .pipe(sass({
+             style: 'compressed',
+             loadPath: [
+                 './resources/sass',
+                 config.bowerDir + '/bootstrap-sass/assets/stylesheets',
+        ]
+                 }) 
+    .on("error", notify.onError(function (error) {
+                         return "Error: " + error.message;
+             }))) 
+             .pipe(gulp.dest('./dist/styles/css')); 
+    });
+    gulp.task('watch', function() {
+         gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+    });
+
+      gulp.task('default', ['bower', 'icons', 'css']);
 });
